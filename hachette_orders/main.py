92ae7c44ba7eb98f5d45f5fd827230b274975e0a
Barrett Hooper <r2277388+github@gmail.com>
2024-10-31 12:50:26 -0700

    working on hachette orders code

--- hachette_orders/main.py
+++ hachette_orders/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from loader.load_ho import upload_ho
 from order_type_regular import calculate_est_ship_date_regular
 from order_type_backordered import calculate_est_ship_date_backordered
-from order_type_released import calculate_est_ship_date_released
+from hachette_orders.order_type_released_soft_allocated import calculate_est_ship_date_released
 from order_type_hold import calculate_est_ship_date_hold
 
 # Load the DataFrame
--- hachette_orders/order_type_released_soft_allocated.py
+++ hachette_orders/order_type_released_soft_allocated.py
@@ -1,30 +1,59 @@
 import pandas as pd
 from function import adjust_to_weekday,next_three_days
+from list_provider import hbg_tier1,hbg_tier2,hbg_tier3,hbg_tier4,hbg_tier5,big_box_stores
 
 def calculate_est_ship_date_released(df):
     todays_date = pd.Timestamp('today').normalize()
-    # Future OSD titles
-    # Big Box Stores
-    big_box_stores = list('Readerlink','Barnes & Noble','Amazon.com','Target Direct','Ingram')
     
-    # Rules for Big Box Stores
-    if (df.osd > todays_date) and (df.STATE in ['CA','WA','OR','NV']) and (df.SSR_Row in big_box_stores):
+    ######################
+    # PRE-OSD Rules
+    ######################
+    # PRE-OSD Rules for Big Box Stores
+    if (df.osd > todays_date) and (df.STATE in hbg_tier1) and (df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=10) 
+        return adjust_to_weekday(estimated_date)
+    
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier2) and (df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=12) 
+        return adjust_to_weekday(estimated_date)
+    
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier3) and (df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=13) 
+        return adjust_to_weekday(estimated_date)
+    
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier4) and (df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=13) 
+        return adjust_to_weekday(estimated_date)
+
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier5) and (df.SSR_Row in big_box_stores):
         estimated_date =  df.osd - pd.DateOffset(days=14) 
         return adjust_to_weekday(estimated_date)
     
-    elif (df.osd > todays_date) and (df.STATE in ['CA','WA']) and (df.SSR_Row in big_box_stores):
-        estimated_date =  df.osd - pd.DateOffset(days=7) 
+    # PRE-OSD Rules for Not Big Box Stores
+    if (df.osd > todays_date) and (df.STATE in hbg_tier1) and (~df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=10) 
         return adjust_to_weekday(estimated_date)
     
-    # Rules for Not Big Box Stores
-    elif (df.osd > todays_date) and (df.STATE in ['CA','WA']) and (~df.SSR_Row in big_box_stores):
-        estimated_date =  df.osd - pd.DateOffset(days=6) # 5 day shipping
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier2) and (~df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=12) 
         return adjust_to_weekday(estimated_date)
     
-    elif (df.osd > todays_date) and (df.STATE in ['AL','NY','NJ','PA','WI']) and (~df.SSR_Row in big_box_stores):
-        estimated_date =  df.osd - pd.DateOffset(days=3) # 5 day shipping
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier3) and (~df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=13) 
+        return adjust_to_weekday(estimated_date)
+    
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier4) and (~df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=13) 
+        return adjust_to_weekday(estimated_date)
+
+    elif (df.osd > todays_date) and (df.STATE in hbg_tier5) and (~df.SSR_Row in big_box_stores):
+        estimated_date =  df.osd - pd.DateOffset(days=14) 
         return adjust_to_weekday(estimated_date) 
-    
+
+######################
+# POST OSD Rules
+######################
+
     # READERLINK RELEASED
     elif (df.SSR_Row == 'Readerlink') and (df.WMSDoNotDeliverAfter is not None) and (df.STATE == 'IL'):
         estimated_date =  df.WMSDoNotDeliverAfter + pd.DateOffset(days=6) # 1 day shipping
@@ -56,11 +85,11 @@ def calculate_est_ship_date_released(df):
         estimated_date = df.WMSDoNotDeliverAfter
         return adjust_to_weekday(estimated_date)
     
-    elif (df.SSR_Row == 'Amazon.com') and (df.WMSDoNotShipAfter is not None) and (df.STATE in ['IN', 'PA']):
+    elif (df.SSR_Row == 'Amazon.com') and (df.WMSDoNotShipAfter is not None) and (df.STATE =='PA'):
         estimated_date = df.WMSDoNotShipAfter - pd.DateOffset(days=1)
         return adjust_to_weekday(estimated_date)
     
-    elif (df.SSR_Row == 'Amazon.com') and (df.WMSDoNotShipAfter is not None) and (df.STATE in ['TN']):
+    elif (df.SSR_Row == 'Amazon.com') and (df.WMSDoNotShipAfter is not None) and (df.STATE == 'TN'):
         estimated_date = df.WMSDoNotShipAfter - pd.DateOffset(days=2)
         return adjust_to_weekday(estimated_date)
     
