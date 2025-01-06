7f2cf3938fe9a9417bc73d5a7f59d957a9a2ba06
Barrett Hooper <r2277388+github@gmail.com>
2025-01-06 13:03:14 -0800

    updating folder for pre-rders from 2024 to 2025

--- amazon_preorders/main.py
+++ amazon_preorders/main.py
@@ -7,7 +7,7 @@ def main():
     df = merge_catalog_inventory()  # Ensure this function does not require arguments or modify as needed
 
     # Define the folder path where the Excel files will be saved
-    folder_path = Path(r'G:\SALES\Amazon\PREORDERS\2024')
+    folder_path = Path(r'G:\SALES\Amazon\PREORDERS\2025')
     
     # Save the DataFrame to Excel
     save_to_excel(df, folder_path)
