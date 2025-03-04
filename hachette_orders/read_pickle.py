14bf51d003489fdb0458ad862f671749d7ffd45b
Barrett Hooper <r2277388+github@gmail.com>
2025-03-04 10:18:35 -0800

    updates for 2025

--- hachette_orders/read_pickle.py
+++ hachette_orders/read_pickle.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import os
 
-file_name = r'C:\Users\rbh\code\hachette_orders\ho_20250220_0825.pkl'
+file_name = fr'C:\Users\rbh\code\ho_20250226_1414.pkl'
+
+base_name = os.path.basename(file_name)
+base_data = base_name.replace('.pkl', '')
+
+print(f'Loading {base_data}...')
+
 df = pd.read_pickle(file_name)
 
 print(df.info())
 
 print(df.head())
 
-excel_file_name = fr'C:\Users\rbh\Desktop\ho_20250220_0825.xlsx'
+excel_file_name = fr'C:\Users\rbh\Desktop\{base_data}.xlsx'
 
 df.to_excel(excel_file_name, index=False)
+
+print(f'Excel Version of Hachette Orders saved to: {excel_file_name}')
