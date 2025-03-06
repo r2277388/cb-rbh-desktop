bdeea38797910c80ef61007adc48cc35dc47faad
Barrett Hooper <r2277388+github@gmail.com>
2025-03-06 12:11:10 -0800

    ük rolling saved to new year

--- UK_Rolling_File_Combining/paths.py
+++ UK_Rolling_File_Combining/paths.py
@@ -1,6 +1,6 @@
 # Set up paths and load data
 folder_path = r"C:\Users\dag\Desktop\Python Scripts\A&CB Data - Python\text_file_deposit"
-output_path = r'G:\SALES\2024 Sales Reports\Sell-Through Reporting\Abrams & Chronicle\Title Sales Week ##.xlsx'
+output_path = r'G:\SALES\2025 Sales Reports\Sell-Through Reporting\Abrams & Chronicle\Title Sales Week ##.xlsx'
 
 
 # folder_path = r"C:\Users\rbh\Desktop\text_file_deposit"
