a7325a517525c3cd46e36dc5cc83a599e6b6150a
Barrett Hooper <r2277388+github@gmail.com>
2025-04-08 11:35:58 -0700

    updates

--- UK_Rolling_File_Combining/paths.py
+++ UK_Rolling_File_Combining/paths.py
@@ -5,5 +5,5 @@ folder_path = fr"C:\Users\dag\Desktop\Python Scripts\A&CB Data - Python\text_fil
 output_path = fr'G:\SALES\2025 Sales Reports\Sell-Through Reporting\Abrams & Chronicle\Title Sales Week ##.xlsx'
 
 
-# folder_path = r"C:\Users\rbh\Desktop\text_file_deposit"
+#folder_path = fr"C:\Users\rbh\Desktop\text_file_deposit"
 # output_path = r"C:\Users\rbh\Desktop\Title Sales Week ##.xlsx"
