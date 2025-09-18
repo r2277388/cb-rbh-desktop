63a2c60988440839479f007b491bebeb9608e70a
Barrett Hooper <r2277388+github@gmail.com>
2025-09-18 15:26:46 -0700

    updated uk rolling location of file

--- UK_Rolling_File_Combining/paths.py
+++ UK_Rolling_File_Combining/paths.py
@@ -1,6 +1,6 @@
 # Set up paths and load data
 # 
-folder_path = fr"C:\Users\dag\OneDrive - chroniclebooks.com\Desktop\Python Scripts\A&CB Data - Python\text_file_deposit"
+folder_path = fr'F:\ANALYSIS\Finance\DataWarehouse\Weekly reports\2025\Abrams & Chronicle\Script Files'
 
 output_path = fr'G:\SALES\2025 Sales Reports\Sell-Through Reporting\Abrams & Chronicle\Title Sales Week ##.xlsx'
 
