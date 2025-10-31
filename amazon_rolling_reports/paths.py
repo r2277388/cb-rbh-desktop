aa236ab4841d59805e16e73a484620d9c6ff462e
Barrett Hooper <r2277388+github@gmail.com>
2025-10-31 13:37:54 -0700

    Normalize line endings

--- amazon_rolling_reports/paths.py
+++ amazon_rolling_reports/paths.py
@@ -1,19 +1,18 @@
-desktop_path = fr"C:\Users\rbh\Desktop"
+desktop_path = r"C:\Users\rbh\Desktop"
 
-amazon_rolling_folder = fr"G:\SALES\2025 Sales Reports\Sell-Through Reporting\Amazon"
+amazon_rolling_folder = r"G:\SALES\2025 Sales Reports\Sell-Through Reporting\Amazon"
 
-amazon_po_folder = fr"G:\SALES\Amazon\PURCHASE ORDERS\2025"
+amazon_po_folder = r"G:\SALES\Amazon\PURCHASE ORDERS\2025"
 
 # Save off files into the Amazon DP Folders
 dp_folders = {
-    "Galison": fr"G:\SALES\Distribution_Partners\Galison\GA REPORTS\Amazon\2025",
-    "Hardie Grant": fr"G:\SALES\Distribution_Partners\Hardie Grant\HG REPORTS\Sell Through Reporting\Amazon\2025",
-    "Laurence King": fr"G:\SALES\Distribution_Partners\Laurence King\LK REPORTS\Amazon\2025",
-    "Levine Querido": fr"G:\SALES\Distribution_Partners\Levine Querido\LQ REPORTS\Sell Through Reporting\Amazon\2025",
-    "Paperblanks": fr"G:\SALES\Distribution_Partners\Paperblanks\PB REPORTS\Sell-Through Reporting\Amazon\2025",
-    "Quadrille": fr"G:\SALES\Distribution_Partners\Quadrille\QD REPORTS\Sell Through Reporting\Amazon\2025",
-    "Sierra Club": fr"G:\SALES\Distribution_Partners\Sierra Club\SC REPORTS\Amazon\2025",
-    "Tourbillon": fr"G:\SALES\Distribution_Partners\Twirl\TW-REPORTS\Sell Through Reporting\Amazon\2025",
-    "Creative Company": fr"G:\SALES\Distribution_Partners\Creative Company\CC REPORTS\Amazon\2025"
-    }
-
+    "Galison": r"G:\SALES\Distribution_Partners\Galison\GA REPORTS\Amazon\2025",
+    "Hardie Grant": r"G:\SALES\Distribution_Partners\Hardie Grant\HG REPORTS\Sell Through Reporting\Amazon\2025",
+    "Laurence King": r"G:\SALES\Distribution_Partners\Laurence King\LK REPORTS\Amazon\2025",
+    "Levine Querido": r"G:\SALES\Distribution_Partners\Levine Querido\LQ REPORTS\Sell Through Reporting\Amazon\2025",
+    "Paperblanks": r"G:\SALES\Distribution_Partners\Paperblanks\PB REPORTS\Sell-Through Reporting\Amazon\2025",
+    "Quadrille": r"G:\SALES\Distribution_Partners\Quadrille\QD REPORTS\Sell Through Reporting\Amazon\2025",
+    "Sierra Club": r"G:\SALES\Distribution_Partners\Sierra Club\SC REPORTS\Amazon\2025",
+    "Tourbillon": r"G:\SALES\Distribution_Partners\Twirl\TW-REPORTS\Sell Through Reporting\Amazon\2025",
+    "Creative Company": r"G:\SALES\Distribution_Partners\Creative Company\CC REPORTS\Amazon\2025",
+}
